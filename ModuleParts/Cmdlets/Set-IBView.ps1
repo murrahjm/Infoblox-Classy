2ffdf6938ce8626b47fa74de30b33a6a5ae5836b
Murrah <JMMURRAH@eprod.com>
2016-12-05 09:00:27 -0600

    updated help for Set-IBView cmdlet

--- ModuleParts/Cmdlets/Set-IBView.ps1
+++ ModuleParts/Cmdlets/Set-IBView.ps1
@@ -1,66 +1,56 @@
 <#
 .Synopsis
-	Set-IBDNSARecord modifies properties of an existing DNS A Record in the Infoblox database.
+	Set-IBView modifies properties of an existing View or NetworkView object in the Infoblox database.
 .DESCRIPTION
-	Set-IBDNSARecord modifies properties of an existing DNS A Record in the Infoblox database.  Valid IB_DNSARecord objects can be passed through the pipeline for modification.  A valid reference string can also be specified.  On a successful edit no value is returned unless the -Passthru switch is used.
+	Set-IBView modifies properties of an existing View or NetworkView object in the Infoblox database.  Valid IB_View or IB_NetworkView objects can be passed through the pipeline for modification.  A valid reference string can also be specified.  On a successful edit no value is returned unless the -Passthru switch is used.
 .PARAMETER Gridmaster
 	The fully qualified domain name of the Infoblox gridmaster.  SSL is used to connect to this device, so a valid and trusted certificate must exist for this FQDN.
 .PARAMETER Credential
 	Powershell credential object for use in authentication to the specified gridmaster.  This username/password combination needs access to the WAPI interface.
 .PARAMETER _Ref
-	The unique reference string representing the DNS record.  String is in format <recordtype>/<uniqueString>:<Name>/<view>.  Value is assigned by the Infoblox appliance and returned with and find- or get- command.
+	The unique reference string representing the View or NetworkView object.  String is in format <recordtype>/<uniqueString>:<Name>/<defaultbool>.  Value is assigned by the Infoblox appliance and returned with and find- or get- command.
 .PARAMETER Record
-	An object of type IB_DNSARecord representing the DNS record.  This parameter is typically for passing an object in from the pipeline, likely from Get-IBDNSARecord.
-.PARAMETER IPAddress
-	The IP Address to set on the provided dns record.  Standard IPv4 notation applies, and a string value must be castable to an IPAddress object.
+	An object of type IB_View or IB_NetworkView representing the View or NetworkView object.  This parameter is typically for passing an object in from the pipeline, likely from Get-IBView.
+.PARAMETER Name
+	The name to set on the provided View or NetworkView object.
 .PARAMETER Comment
-	The comment to set on the provided dns record.  Can be used for notation and keyword searching by Get- cmdlets.
-.PARAMETER TTL
-	The record-specific TTL to set on the provided dns record.  If the record is currently inheriting the TTL from the Grid, setting this value will also set the record to use the record-specific TTL
-.PARAMETER ClearTTL
-	Switch parameter to remove any record-specific TTL and set the record to inherit from the Grid TTL
+	The comment to set on the provided View or NetworkView object.  Can be used for notation and keyword searching by Get- cmdlets.
 .PARAMETER Passthru
-	Switch parameter to return an IB_DNSARecord object with the new values after updating the Infoblox.  The default behavior is to return nothing on successful record edit.
+	Switch parameter to return an IB_View or IB_NetworkView object with the new values after updating the Infoblox.  The default behavior is to return nothing on successful record edit.
 .EXAMPLE
-	Get-IBDNSARecord -Gridmaster $Gridmaster -Credential $Credential -comment 'old comment' -Strict | Set-IBDNSARecord -comment 'new comment'
+	Get-IBView -Gridmaster $Gridmaster -Credential $Credential -comment 'old comment' -Strict | Set-IBView -comment 'new comment'
 	
-	This example retrieves all dns records with a comment of 'old comment' and replaces it with 'new comment'
+	This example retrieves all View or NetworkView objects with a comment of 'old comment' and replaces it with 'new comment'
 .EXAMPLE
-	Get-IBDNSARecord -Gridmaster $Gridmaster -Credential $Credential -Name testrecord.domain.com | Set-IBDNSARecord -IPAddress 192.168.1.2 -comment 'new comment' -passthru
+	Get-IBView -Gridmaster $Gridmaster -Credential $Credential -Name view2 | Set-IBView -name view3 -comment 'new comment' -passthru
 
-		Name      : testrecord.domain.com
-		IPAddress : 192.168.1.1
+		Name      : view3
 		Comment   : new comment
-		View      : default
-		TTL       : 0
-		Use_TTL   : False
-		_ref      : record:a/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkYWR1dGwwMWNvcnAsMTAuOTYuMTA1LjE5MQ:testrecord.domain.com/default
+		is_default: false
+		_ref      : view/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkYWR1dGwwMWNvcnAsMTAuOTYuMTA1LjE5MQ:view3/false
 
 	description
 	-----------
-	This example modifes the IPAddress and comment on the provided record and outputs the updated record definition
+	This example modifes the name and comment on the provided record and outputs the updated record definition
 .EXAMPLE
-	Set-IBDNSARecord -Gridmaster $Gridmaster -Credential $Credential -_ref record:a/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkZGNlcGQwMWhvdW1yaWIsMTAuNzUuMTA4LjE4MA:testrecord2.domain.com/default -ClearTTL -Passthru
+	Set-IBView -Gridmaster $Gridmaster -Credential $Credential -_ref networkview/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkZGNlcGQwMWhvdW1yaWIsMTAuNzUuMTA4LjE4MA:networkview2/false -Passthru -comment $False
 
-		Name      : testrecord2.domain.com
-		IPAddress : 192.168.1.2
-		Comment   : new record
-		View      : default
-		TTL       : 0
-		Use_TTL   : False
-		_ref      : record:a/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkZGNlcGQwMWhvdW1yaWIsMTAuNzUuMTA4LjE4MA:testrecord2.domain.com/default
+		Name      : networkview2
+		Comment   : 
+		is_default: False
+		_ref      : networkview/ZG5zLmJpbmRfYSQuX2RlZmF1bHQuY29tLmVwcm9kLHBkZGNlcGQwMWhvdW1yaWIsMTAuNzUuMTA4LjE4MA:networkview2/false
 
 	description
 	-----------
-	This example finds the record based on the provided ref string and clears the record-specific TTL
+	This example finds the record based on the provided ref string and clears the comment
 .INPUTS
-	System.Net.IPAddress[]
 	System.String
 	IB_ReferenceObject
 .OUTPUTS
-	IB_DNSARecord
+	IB_View
+    IB_NetworkView
 #>
-Function Set-IBDNSARecord{
+Function Set-IBView{
     [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact="High")]
     Param(
         [Parameter(Mandatory=$True,ValueFromPipelinebyPropertyName=$True)]
