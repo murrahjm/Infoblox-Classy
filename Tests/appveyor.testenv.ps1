81dd066a4175c321cdbad51d7c7162786ca3fcc0
Murrah <JMMURRAH@eprod.com>
2017-01-03 16:02:50 -0600

    corrected path reference to azuredeploy.json file

--- Tests/appveyor.testenv.ps1
+++ Tests/appveyor.testenv.ps1
@@ -19,9 +19,9 @@ $SecureIBAdminPassword = $env:IBAdminPassword | ConvertTo-SecureString -AsPlainT
 If (!(Get-azurermresourcegroup $rgname -ea 'silentlycontinue')){
     $ResourceGroup = New-AzureRMResourceGroup -Name $RGName -Location $Location
 }
-$TestResult = test-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile "$Scriptlocation\AzureDeploy.json" -virtualMachines_TestGridmaster_adminPassword $SecureIBAdminPassword
+$TestResult = test-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile ".\AzureDeploy.json" -virtualMachines_TestGridmaster_adminPassword $SecureIBAdminPassword
 If ($Testresult.count -eq 0){
-    $Result = New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile "$Scriptlocation\AzureDeploy.json" -virtualMachines_TestGridmaster_adminPassword $SecureIBAdminPassword
+    $Result = New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile ".\AzureDeploy.json" -virtualMachines_TestGridmaster_adminPassword $SecureIBAdminPassword
     If ($result.ProvisioningState -eq 'Succeeded'){
         $AdminCredential = new-object -TypeName system.management.automation.pscredential -ArgumentList 'admin', $SecureIBAdminPassword
         $GridmasterFQDN = "$($result.parameters.virtualMachines_TestGridmaster_name.value).$($result.parameters.location.value).cloudapp.azure.com"
