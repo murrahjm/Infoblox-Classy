b046fe44803946e5dab26c8976d9b0ecf492572f
murrahjm <murrahjm@gmail.com>
2017-01-10 10:50:13 -0600

    added step to copy psd1 file to module folder

--- build.ps1
+++ build.ps1
@@ -6,11 +6,11 @@ $ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
 #create module folder
 new-item -Path $ProjectRoot -name $ModuleName -ItemType Directory
 
-#concat moduleparts files
+#build module files
 $Scripts = Get-ChildItem "$projectRoot\ModuleParts" -Filter *.ps1 -Recurse
 $FunctionstoExport = $(get-childitem).name.replace('.ps1','')
 $Scripts | get-content | out-file -FilePath "$projectRoot\$ModuleName\$ModuleName.psm1"
-
+copy-item $projectroot\infoblox.psd1 $projectroot\$modulename\$modulename\psd1
 #Update module manifest
 $modulemanifestdata = @{
     Author = $ENV:APPVEYOR_ACCOUNT_NAME
